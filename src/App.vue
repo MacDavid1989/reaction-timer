b12a144d515274f0a9124c0c8d5837ef053ca15e
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-22 18:41:37 -0400

    create two components Block and Result, create a button in App that will start the game, add a method Start to handle this initial data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <h1>Reaction Timer</h1>
+  <button @click="start">Play</button>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = !this.isPlaying;
+      this.delay = 2000 + Math.random() * 5;
+    },
+  },
 };
 </script>
