ecbb21416d72fea9c721037b7aa0f9d2c4635557
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-22 19:19:00 -0400

    emit reactionTime data to App component, listen for the event from the Block component and call endGame method to change data in App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p>Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
@@ -14,12 +15,17 @@ export default {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
     };
   },
   methods: {
     start() {
       this.isPlaying = !this.isPlaying;
       this.delay = 2000 + Math.random() * 5000;
+    },
+    endGame(time) {
+      this.score = time;
+      this.isPlaying = !this.isPlaying;
     },
   },
 };
